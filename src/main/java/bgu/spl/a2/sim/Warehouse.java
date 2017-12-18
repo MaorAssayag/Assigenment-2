51c20842846c782d28dfb523ea91232b8090c191
Maor Assayag <maorassayag16@gmail.com>
2017-12-18 23:24:07 +0200

    done

--- src/main/java/bgu/spl/a2/sim/Warehouse.java
+++ src/main/java/bgu/spl/a2/sim/Warehouse.java
@@ -1,10 +1,44 @@
 package bgu.spl.a2.sim;
 
+
 /**
  * represents a warehouse that holds a finite amount of computers
  * and their suspended mutexes.
  * releasing and acquiring should be blocking free.
  */
 public class Warehouse {
+	
+	private Computer computers[];
+	private int currentCapacity;
+	
+	public Warehouse(int capacity) {
+		this.computers = new Computer[capacity];
+		this.currentCapacity = capacity;
+	}
+	
+	public void addComputer(Computer computer) {
+		if (currentCapacity > 0) {
+			this.computers[this.computers.length - currentCapacity] = computer;
+			currentCapacity--;
+		}
+	}
+	
+	public Computer getComputer(String computerType) {
+		for(Computer computer : this.computers) {
+			if (computer.computerType.equals(computerType)){
+				return computer;
+			}
+		}
+		return null;
+	}
+	
+	public SuspendingMutex getSuspendingMutex(String computerType) {
+		for(Computer computer : this.computers) {
+			if (computer.computerType.equals(computerType)){
+				return computer.suspendingMutex;
+			}
+		}
+		return null;
+	}
 	
 }
